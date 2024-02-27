6835336bbfa9bc5121017a4157ee9d85eed6f03b
Ricardo Barra Morales <rbarramorales@gmail.com>
2024-02-27 20:00:54 +0100

    Added greetness2 dependency

--- source/main.cpp
+++ source/main.cpp
@@ -3,10 +3,17 @@
 
 #include "lib.hpp"
 
+#include "greetness2/greetness2.hpp"
+
 auto main() -> int
 {
   auto const lib = library {};
   auto const message = "Hello from " + lib.name + "!";
   std::cout << message << '\n';
+
+  const exported_class greeter {};
+  auto const message2 = "Hello from " + std::string(greeter.name()) + "!";
+  std::cout << message2 << '\n';
+
   return 0;
 }
